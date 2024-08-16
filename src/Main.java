bd9b4294d76d6b0d47a5698a637ff14b5a3e7300
Giovanni Silva <git@gsilva.pro>
2024-08-15 22:09:12 -0300

    chamada com metodo não estatico e nova classe

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,21 @@ public class Main {
     public static String FUNCTION_NAME = "Main function";
 
 
-    // Exemplo de chamada de methodo estatico
+    // Exemplo de chamada de methodo não estatica
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        var somatorio = soma(2, 4);
-        int subtracao = subtrair(4, 5);
+        // instanciando objetos do tipo MinhaCalculadora
+        var objetoCalculadora = new MinhaCalculadora();
+        MinhaCalculadora objetoCalculadora2 = new MinhaCalculadora();
+
+        // Acessando methodos dentro dos objetos
+        var somatorio = objetoCalculadora.soma(2, 4);
+        int subtracao = objetoCalculadora2.subtrair(4, 5);
+
         System.out.println("O somatorio é: " + somatorio);
         System.out.println("A subtração é " + subtracao);
 
     }
 
-    public static int soma(int a, int b) {
-        return a + b;
-    }
-
-    public static int subtrair(int a, int b) {
-        return a - b;
-    }
 
 }
---
+++ src/MinhaCalculadora.java
@@ -0,0 +1,11 @@
+public class MinhaCalculadora {
+
+    public int soma(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+}
