853462f4bd048fd7f076f3804e37092f327207e0
Giovanni Silva <git@gsilva.pro>
2024-08-16 21:53:54 -0300

    aula 2 lendo informações do teclado e executando operaçãos com if else

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     // Exemplo de chamada de methodo não estatica
@@ -7,16 +10,44 @@ public class Main {
         System.out.println("1. Somar");
         System.out.println("2. Subtrair");
 
-        // instanciando objetos do tipo MinhaCalculadora
-//        var objetoCalculadora = new MinhaCalculadora();
-//        MinhaCalculadora objetoCalculadora2 = new MinhaCalculadora();
-//        // Acessando methodos dentro dos objetos
-//        var somatorio = objetoCalculadora.soma(2, 4);
-//        int subtracao = objetoCalculadora2.subtrair(4, 5);
-//
-//        System.out.println("O somatorio é: " + somatorio);
-//        System.out.println("A subtração é " + subtracao);
+        // Pego o numero digitado no teclado
+        Scanner scanner = new Scanner(System.in);
+        Integer opcao = 0;
+        try {
+            opcao = scanner.nextInt();
+            MinhaCalculadora calculadora = new MinhaCalculadora();
 
+            if(opcao == 1) {
+                System.out.println("Soma escolhida.");
+                ValoresDigitados valoresDigitados = lerValores();
+                Integer resultado = calculadora.soma(valoresDigitados.valorA, valoresDigitados.valorB);
+                System.out.println("Resultado: " + resultado);
+            }else if (opcao == 2) {
+                System.out.println("Subtracao escolhida. Digite o primeiro valor: ");
+                ValoresDigitados valoresDigitados = lerValores();
+                Integer resultado = calculadora.subtrair(valoresDigitados.valorA, valoresDigitados.valorB);
+                System.out.println("Resultado: " + resultado);
+            } else {
+                System.out.println("Opção inválida, favor digite novamente");
+            }
+
+        } catch (InputMismatchException exception){
+            System.out.println("Valor invalido, favor digitar um numero");
+        }
+
+
+    }
+
+    public static ValoresDigitados lerValores() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro valor: ");
+        Integer valorA = scanner.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        Integer valorB = scanner.nextInt();
+        ValoresDigitados valoresDigitados = new ValoresDigitados(valorA, valorB);
+//        valoresDigitados.valorA = valorA;
+//        valoresDigitados.valorB = valorB;
+        return valoresDigitados;
     }
 
 
